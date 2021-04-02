78c79a2ccfc2f72cb72cdb44461a95743834ae92
ramesh-DevOps <73949769+ramesh-DevOps@users.noreply.github.com>
2021-04-02 11:43:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,6 +39,13 @@ resource "aws_security_group" "Instance_SG" {
     protocol    = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    description = "instances aws security group"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
